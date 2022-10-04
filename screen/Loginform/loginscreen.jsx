3cebd01ea9d19f4b55483fd60cb62f2a5ab8ca8b
Mish880 <Pereraittec6@gmail.com>
2022-10-04 13:32:51 +0530

    Update

--- screen/Loginform/loginscreen.jsx
+++ screen/Loginform/loginscreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View,TextInput,StyleSheet,Button } from 'react-native';
+import { Text, View,TextInput,StyleSheet,Image,TouchableOpacity} from 'react-native';
 
 const Loginscreen = () => {
  
@@ -7,25 +7,80 @@ const Loginscreen = () => {
     return (
       
       <View>
-            <TextInput style={styles.input} placeholder="NIC" keyboardType="numeric" />
-            
-          
-      
+             <Text style={styles.text}>LOGIN FORM</Text>
+             
+             <View style={styles.inputfields}>
+              <TextInput style={styles.textinput} placeholder="User Name" keyboardType="text" />
+              <TextInput style = {styles.textinput} placeholder="Passwoed" returnKeyType='go' secureTextEntry autoCorrect={false}
+
+/>
+</View>
+            <TouchableOpacity style={styles.buttonfield}>
+                <Text style={styles.Btext}>Login</Text>
+            </TouchableOpacity>
       </View>
       
    
     );
        
-  }
+  };
+
 const styles = StyleSheet.create({
-    input: {
-    height: 50,
-    marginTop: 25,
-    borderRadius:30,
-    borderWidth: 1,
-    padding: 10,
-    width:280
+   
+  textinput: {
+      height:50,
+      margintop:25,
+      borderRadius:30,
+      borderWidth: 1,
+      padding: 10,
+      width:280
   },
-})
+
+  tinylogo:{
+      width:100,
+      height:100,
+  },
+
+  container:{
+      marginTop:20,
+      alignItems:'center',
+      justifyContent:'center',
+  },
+
+   text: {
+      frontWeight:'bold',
+      textAlign:'center',
+      justifyContent:'center',
+      alignItems:'center',
+      color:'black',
+      fontSize:30
+    },
+
+    inputfields:{
+        justifyContent:'center',
+        textAlign:'center',
+        marginLeft:50
+    },
+
+    buttonfield:{
+       width:'50%',
+       padding:5,
+       fontSize:20,
+       backgroundColor:"black",
+       height:40,
+       margin:78,
+       
+       alignItems:'center',
+       justifyContent:'center',
+       marginTop:'3%',
+       borderRadius:100
+    },
+
+    Btext:{
+      color:'white',
+      textAlign:'center',
+      fontWeight:'bold',
+    },
+});
 
 export default Loginscreen;
