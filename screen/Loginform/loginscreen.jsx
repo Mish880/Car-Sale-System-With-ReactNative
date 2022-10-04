fa768525c29be1d99cf0aad63c5e1907a8debe40
Mish880 <Pereraittec6@gmail.com>
2022-10-04 11:50:57 +0530

    Update

--- screen/Loginform/loginscreen.jsx
+++ screen/Loginform/loginscreen.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,TextInput,StyleSheet,Button } from 'react-native';
 
 const Loginscreen = () => {
-  return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>Hello, world!</Text>
-    </View>
-  );
-}
+ 
+
+    return (
+      
+      <View>
+            <TextInput style={styles.input} placeholder="NIC" keyboardType="numeric" />
+            
+          
+      
+      </View>
+      
+   
+    );
+       
+  }
+const styles = StyleSheet.create({
+    input: {
+    height: 50,
+    marginTop: 25,
+    borderRadius:30,
+    borderWidth: 1,
+    padding: 10,
+    width:280
+  },
+})
 
 export default Loginscreen;
