325f5d4a775ec17ef3214e69713983eaf2dcacd8
Mish880 <Pereraittec6@gmail.com>
2022-11-01 09:11:08 +0530

    Update React Mobile

--- App.js
+++ App.js
@@ -11,9 +11,32 @@ import ImageUploadfrom from './screen/ImageUploadform/imageupload'
 const App = () => {
     return (
        <View>
-               <Loginform/>
+              <Vehicleform/>
        </View>
     );
 };
 
 export default App;
+
+
+
+/*import ImageUploadfrom from './screen/ImageUploadform/imageupload'
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>{<Stack.Navigator>
+        <Stack.Screen name="Signupform" component={Signupform} />
+        <Stack.Screen name="Loginform" component={Loginform} />
+        <Stack.Screen name="Customerform" component={Customerform} />
+        <Stack.Screen name="Vehicleform" component={Vehicleform} />
+      </Stack.Navigator>}</NavigationContainer>
+  );
+}*/ 
